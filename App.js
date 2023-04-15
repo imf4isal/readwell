7fff880420412bff36ffad89e0c0fa8c85213e45
Faisal <md.faisal1899@yahoo.com>
2023-04-15 06:30:51 +0600

    implement product card(minimal)

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import { StyleSheet } from 'react-native';
-import ProductImageScreen from './app/screen/ProductImageScreen';
+import AltCard from './app/components/AltCard';
 
 export default function App() {
-    return <ProductImageScreen />;
+    return (
+        <AltCard
+            title="Book"
+            subtitle="BDT 200"
+            image={require('./app/assets/products/organizedbook.jpg')}
+        />
+    );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        margin: 50,
+    },
+});
---
+++ app/components/AltCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+import colors from '../config/colors';
+
+function AltCard({ title, subtitle, image }) {
+    return (
+        <View style={styles.container}>
+            <Image source={image} style={styles.image} />
+            <View style={styles.detailContainer}>
+                <Text style={styles.title}>{title}</Text>
+                <Text>{subtitle}</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.light,
+        borderRadius: 10,
+        margin: 20,
+        marginTop: 100,
+        overflow: 'hidden',
+    },
+    detailContainer: {
+        padding: 10,
+        marginBottom: 5,
+    },
+    image: {
+        width: '100%',
+        height: 200,
+    },
+    title: {
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+});
+
+export default AltCard;
