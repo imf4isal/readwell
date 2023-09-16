87e083f321884ee216e428f5c2e3e6d9cf39db9c
Faisal <md.faisal1899@yahoo.com>
2023-09-16 09:53:24 +0600

    add new colors into color configeration

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,4 +1,14 @@
 export default {
-    primary: '#fc5c65',
-    secondary: '#4ecdc4',
+    primary: '#e97777',
+    primaryB: '#E46969',
+    primaryC: '#EA5455',
+    secondary: '#7db9b6',
+    secondaryB: '#58A9A5',
+    secondaryC: '#60C9C4',
+    black: '#000',
+    white: '#fff',
+    light: '#f8f4f4',
+    dark: '#002B5B',
+    danger: '#ff5252',
+    grey: 'grey',
 };
