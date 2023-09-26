98b9ca91431573682993eafe066843d6b3556f5e
Faisal <md.faisal1899@yahoo.com>
2023-09-26 16:53:00 +0600

    set api layer for adding book

--- app/api/listings.js
+++ app/api/listings.js
@@ -4,6 +4,25 @@ const endpoint = '/listings';
 
 const getListings = () => client.get(endpoint);
 
+const addListing = (listing) => {
+    const data = new FormData();
+    data.append('title', listing.title);
+    data.append('price', listing.price);
+    data.append('categoryId', listing.category.value);
+    data.append('description', listing.description);
+
+    listing.images.forEach((image, index) =>
+        data.append('images', {
+            name: 'image' + index,
+            type: 'image/jpeg',
+            uri: image,
+        })
+    );
+
+    return client.post(endpoint, data);
+};
+
 export default {
     getListings,
+    addListing,
 };
