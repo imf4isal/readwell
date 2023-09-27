462085a07509a848cd3eabf7a8ab5239e48f07d1
Faisal <md.faisal1899@yahoo.com>
2023-09-27 10:11:29 +0600

    set up cache layer

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './app/navigation/AppNavigator';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <AppNavigator />
-        </NavigationContainer>
-    );
+    const demo = async () => {
+        try {
+            await AsyncStorage.setItem('person', JSON.stringify({ id: 1 }));
+            const value = await AsyncStorage.getItem('person');
+
+            const person = JSON.parse(value);
+            console.log(person);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    demo();
+
+    return null;
 }
